a386e59cf6754a051c6481b936e7f8f5e1a19990
mirhanbeydogan56 <mirhanbeydogan56@gmail.com>
2025-12-23 15:59:06 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,23 @@
-<script type="module">
 import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
-export const firebaseConfig={apiKey:"AIzaSyASo730FXSqQx4TRhFRAQVYn9Eetyjri_s",authDomain:"bd-team-30e74.firebaseapp.com",databaseURL:"https://bd-team-30e74-default-rtdb.firebaseio.com",projectId:"bd-team-30e74"};
+
+export const firebaseConfig={
+  apiKey:"AIzaSyASo730FXSqQx4TRhFRAQVYn9Eetyjri_s",
+  authDomain:"bd-team-30e74.firebaseapp.com",
+  databaseURL:"https://bd-team-30e74-default-rtdb.firebaseio.com",
+  projectId:"bd-team-30e74"
+};
+
 export const app=getApps().length?getApps()[0]:initializeApp(firebaseConfig);
 export const auth=getAuth(app);
 export const db=getDatabase(app);
-export const api={signInWithEmailAndPassword,onAuthStateChanged,ref,push,onValue,update};
-</script>
+
+export const api={
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  ref,
+  push,
+  onValue,
+  update
+};
